4a08873dc0034408ac48e555222dd8b91167f954
Abdessamad <abdessamadkhelifaouared@gmail.com>
2026-03-12 18:32:04 +0100

    fix weather API response structure

--- api/weather.js
+++ api/weather.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
   const API_KEY = process.env.WEATHER_KEY;
   const city = req.query.city;
 
-  // check if city is provided
   if (!city) {
     return res.status(400).json({
       error: "City parameter is required"
@@ -12,13 +11,21 @@ export default async function handler(req, res) {
 
   try {
 
-    const weatherResponse = await fetch(
+    const weatherRes = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
     );
 
-    const weatherData = await weatherResponse.json();
+    const forecastRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`
+    );
 
-    res.status(200).json(weatherData);
+    const weatherData = await weatherRes.json();
+    const forecastData = await forecastRes.json();
+
+    res.status(200).json({
+      weatherData: weatherData,
+      forecast: forecastData
+    });
 
   } catch (error) {
 
